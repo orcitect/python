d9efb84db632de98fa46fa5b4e8b4f9e093993b4
Oscar Petersén <orcitect@runbox.com>
2019-02-13 21:21:06 +0100

    Update count_set_bits.py
    expanded example

--- count_set_bits.py
+++ count_set_bits.py
@@ -1,12 +1,12 @@
-def bit_counter():
+def bit_counter(num):
 
     count = 0
 
-    for i in range(1000000):
+    for i in range(num):
         count += f'{i:b}'.count('1')
 
     print(count)
 
 
 if __name__ == '__main__':
-    bit_counter()
+    bit_counter(1000000)
