5e34af11ac6f4e83a9d1cdc2c6584cb83898b0fd
Oscar Petersén <orcitect@runbox.com>
2019-02-14 22:36:05 +0100

    tweaking

--- maskify.py
+++ maskify.py
@@ -1,14 +1,17 @@
-import re
-
-
 def maskify(cc):
+
+    # replace spaces & dashes
+    # ensure uppercase for consistency
+
+    cc = cc.replace('-', '').replace(' ', '').upper()
+
+    # make an exception for input with <= 6 characters
 
     if len(cc) <= 6:
         print(cc)
-    else:
-        print((len(cc) - 4) * "#" + cc[1:-4])
-
-    #print(cc.replace('-', '').replace(' ', ''))
+    print(cc)
+    # else:
+    #     print((len(cc) - 4) * "#" + cc[1:-4])
 
 
 if __name__ == '__main__':
@@ -17,4 +20,4 @@ if __name__ == '__main__':
     maskify('1111222233334444')
     maskify('1111 2222 3333 4444')
     maskify('ABCD-EFGH-IJKL-MNOP')
-    maskify('ABCD-EFGH-IJKL-MNOP')
+    maskify('1A2B2D3E4F5G6H7k')
