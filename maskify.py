38dd92f1d494ee41777453b7a5d7e3a2d77b7b3d
Oscar Petersen <oscar.petersen@quinyx.com>
2019-02-20 09:45:21 +0100

    Update maskify.py
    updated regex to reduce function calls, rm unneccessary list conversion

--- maskify.py
+++ maskify.py
@@ -3,14 +3,14 @@ import re
 
 def maskify(cc):
 
-    regex = re.compile(r'[0-9]+')
+    regex = re.compile(r'\d+')
 
     cc = cc.replace('-', '').replace(' ', '')
 
     if len(cc) <= 5:
         print(cc)
     else:
-        sub = ''.join([(re.sub(regex, '#', c)) for c in list(cc[1:-4])])
+        sub = ''.join([(re.sub(regex, '#', c)) for c in cc[1:-4]])
         new_cc = cc[0] + sub + cc[-4:]
         print(new_cc)
 
